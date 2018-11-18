92bc2d042b9849a6b14e2cb9987af18c20f6f0ac
Craig Derocher <craigderocher@Craig.local>
2018-11-18 13:18:14 -0600

    Message

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCI9itee9WzvjnmyPU8ghjxAyRYKay_nYU",
+	authDomain: "multiplayer-rps-22496.firebaseapp.com",
+	databaseURL: "https://multiplayer-rps-22496.firebaseio.com",
+	projectId: "multiplayer-rps-22496",
+	storageBucket: "multiplayer-rps-22496.appspot.com",
+	messagingSenderId: "157692586189"
+};
+firebase.initializeApp(config);
